2e3fea4548fa7a0087273b5e5a596198c8eea075
Fideline <fideline_bzn@yahoo.com>
2023-07-25 10:39:30 +0200

    fixing style

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] w-fit p-5 rounded-full m-auto">
+    <div class=" relative shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] w-fit p-5 rounded-full m-auto hover:cursor-pointer">
         <slot></slot>
     </div>
 </template>
@@ -9,3 +9,31 @@ export default {
     name: 'MainButton',
 }
 </script>
+
+<style scoped>
+
+div{
+    transition: all 0.8s ease-out;
+}
+
+div::after{
+    content: " ";
+    background-color: inherit;
+    width: 5px;
+    height: 5px;
+    position: absolute;
+    bottom: -35%;;
+    left: 50%;
+    -ms-transform: translate(-50%);
+    transform: translateX(-50%);
+    border-radius: 50%;
+    transition: all 0.5s ease-out;
+
+}
+
+div:hover:after{
+    background-color: blue;
+}
+
+
+</style>
