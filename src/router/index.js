894364ca957c1592324fa6eb0d2bcc6572153696
Wintxer <47366828+Wintxer@users.noreply.github.com>
2023-07-25 10:40:01 +0200

    fan page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FanView from '../views/FanView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },  
+  {
+    path: '/fan',
+    name: 'fan',
+    component: FanView
   },
   {
     path: '/about',
