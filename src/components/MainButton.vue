ffbd1a90ea6dfd1ab5dda1681aaee4f9dd9e28b2
Fideline <fideline_bzn@yahoo.com>
2023-07-25 10:19:37 +0200

    layout struct

---
+++ src/components/MainButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] w-fit p-5 rounded-full m-auto">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainButton',
+}
+</script>
