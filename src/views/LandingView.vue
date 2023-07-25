32027a3984a54a8764bc0076639e0bbfb842eb23
Fideline <fideline_bzn@yahoo.com>
2023-07-25 14:27:42 +0200

    link to home page

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -26,7 +26,7 @@
                     </div>
                 </div>
                 <div>
-                    <MainButton class="hideAfterElem">
+                    <MainButton>
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"  viewBox="0 0 256 512"><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg>
                     </MainButton>
                 </div>
@@ -35,7 +35,7 @@
         </div>
         <div class="mt-12">
             <MainButton class="!rounded-3x !py-3 flex items-center hideAfterElem"> 
-                Choisir </MainButton>
+                <a href="/home">Choisir</a> </MainButton>
         </div>
     </div>
 </template>
